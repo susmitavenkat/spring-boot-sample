69b03f2bc7ae92da4332120b9d1612dbc4cda64e
susmitavenkat <45465359+susmitavenkat@users.noreply.github.com>
2018-11-29 23:17:17 +0530

    Add files via upload

---
+++ src/main/java/com/recommendationengine/demo/RecommendationEngineApplication.java
@@ -0,0 +1,14 @@
+package com.recommendationengine.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationEngineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationEngineApplication.class, args);
+	}
+}
+
+
+
